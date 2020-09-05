78dd9b7ebc8e9256ccbbecba3e067aeffd793804
m-lobocki <mlobocki@outlook.com>
2020-09-05 18:52:57 +0200

    Add microservice abstraction

--- cart/app.js
+++ cart/app.js
@@ -1,33 +1,6 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const config = require('./config.json');
-const registry = require('../lib/registry');
-const indexRouter = require('./routes/index');
+const routing = require('./routing');
+const microservice = require('../lib/microservice');
 
-const app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-//własne ścieżki
-app.use('/', indexRouter);
-//rejestracja mikroserwisu
-registry.registerService(config, app, express.Router());
-
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500);
-});
-
+const app = microservice(__dirname, config, routing);
 module.exports = app;
